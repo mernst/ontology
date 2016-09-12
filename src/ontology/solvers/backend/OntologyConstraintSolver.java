3903755e73c5eba3b105c7ce0e9038cc11217499
CharlesZ-Chen <charleszhuochen@gmail.com>
2016-09-12 15:10:17 -0400

    update fof adapting cf and cfi

--- src/ontology/solvers/backend/OntologyConstraintSolver.java
+++ src/ontology/solvers/backend/OntologyConstraintSolver.java
@@ -108,10 +108,10 @@ public class OntologyConstraintSolver extends ConstraintSolver {
                     continue;
                 }
 
-//               PreferenceConstraint preferCons = InferenceMain.getInstance().getConstraintManager()
-//               .createPreferenceConstraint((VariableSlot) superType, curBtm, 50);
-               PreferenceConstraint preferCons = new PreferenceConstraint((VariableSlot) superType, curBtm, 50);
-               InferenceMain.getInstance().getConstraintManager().add(preferCons);
+               PreferenceConstraint preferCons = InferenceMain.getInstance().getConstraintManager()
+               .createPreferenceConstraint((VariableSlot) superType, curBtm, 50);
+//               PreferenceConstraint preferCons = new PreferenceConstraint((VariableSlot) superType, curBtm, 50);
+//               InferenceMain.getInstance().getConstraintManager().add(preferCons);
                preferSet.add(preferCons);
             }
         }
